6f5447ba5fffed47b8161e1edf2cb9d07a992685
rjdelrio <rjdelrio@uc.cl>
2021-10-03 23:00:42 -0300

    fix

---
+++ test/general_test.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require_relative 'test_helper'
+require_relative '../lib/minesweeper_model'
+require_relative '../lib/minesweeper_controller'
+require_relative './controller_test'
+require 'test/unit'
+
+class MinesweeperModelTest < Test::Unit::TestCase
+
+  def some_test
+  end
+  
+
+
+end
